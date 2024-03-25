cb5b45b498c3d8600e67289ffb554e53365f66b9
robmab <rob_mb@outlook.es>
2024-03-25 20:02:37 +0100

    Responsive Navbar

--- Front-End/src/js/component/navbar.jsx
+++ Front-End/src/js/component/navbar.jsx
@@ -5,28 +5,16 @@ import "../../styles/component/navbar.css";
 import logo from "../../img/Logo.png";
 import shop from "../../img/Icon_Icon_Shopping_Cart_white.png";
 import user from "../../img/Usser.png";
-
+import search from "../../img/Icon_Search-White.png";
 
 export const Navbar = () => {
-  
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand ">
         {/* LEFT */}
         <a className="navbar-brand" href="#">
           <img src={logo} alt="" width="150" height="65" />
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
 
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
@@ -42,13 +30,22 @@ export const Navbar = () => {
           </li>
         </ul>
 
+        {/* MODAL */}
+
         {/* SEARCH */}
-        <Search/>
+        <Search />
 
         {/* RIGHT  */}
         <div className="rigth-icons">
-          <img src={shop} alt="" height="30px" width="30px" />
-          <img src={user} alt="" height="60px" width="60px" />
+          <img
+            style={{display:"none"}}
+            className="zero"
+            src={search}
+            alt=""
+            width="21px"
+          />
+          <img id="first" src={shop} alt="" height="30px" width="30px" />
+          <img id="second" src={user} alt="" height="60px" width="60px" />
         </div>
       </nav>
     </>
