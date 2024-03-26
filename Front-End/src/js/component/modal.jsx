661bccd85f163b638f863f23dedb991f838c3599
robmab <rob_mb@outlook.es>
2024-03-26 01:03:55 +0100

    hooks

--- Front-End/src/js/component/modal.jsx
+++ Front-End/src/js/component/modal.jsx
@@ -2,28 +2,11 @@ import React, { useRef, useEffect } from "react";
 
 import { Search } from "./search.jsx";
 import "../../styles/component/modal.css";
+import { useOutsideAlerter } from "../hooks/useOutsideAlerter.jsx";
 
 export const Modal = (props) => {
-  /* MODAL REF FUNCTION */
-  function useOutsideAlerter(ref) {
-    useEffect(() => {
-      /* Trigger ouside modal */
-      function handleClickOutside(event) {
-        if (ref.current && !ref.current.contains(event.target)) {
-          props.setShowModal(false);
-        }
-      }
-      // Bind the event listener
-      document.addEventListener("mousedown", handleClickOutside);
-      return () => {
-        // Unbind the event listener on clean up
-        document.removeEventListener("mousedown", handleClickOutside);
-      };
-    }, [ref]);
-  }
-
   const wrapperRef = useRef(null);
-  useOutsideAlerter(wrapperRef);
+  useOutsideAlerter(wrapperRef, props.setShowModal, false);
 
   return (
     <div ref={wrapperRef} className="modal">
